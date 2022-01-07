f8833e8e34d056933c904ea513ab6a65510f27da
Egor-Kozlov <kozlov1998.2010@gmail.com>
2022-01-07 11:20:50 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString().split('').reverse()
+    if (n[n.length - 1] === '-') {
+        n.splice(-1)
+    }
+    return Number(n.join(''))
 }
